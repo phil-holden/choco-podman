3d6c38908e78e7e9151324cfed6c9fef9ae864ed
Phil Holden <pholden77@oulook.com>
2022-06-11 13:51:53 +1000

    feat: initial package setup

---
+++ src/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://github.com/containers/podman/releases/download/$env:ChocolateyPackageVersion/podman-$env:ChocolateyPackageVersion.msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  
+  softwareName  = 'podman*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
