400fc4f9ae162ac2f696b1aa3e685efed904b7a3
Phil Holden <pholden77@oulook.com>
2022-06-13 19:24:30 +1000

    fix: Get-Checksum paths

--- .github/workflows/Get-Checksum.ps1
+++ .github/workflows/Get-Checksum.ps1
@@ -1,10 +1,10 @@
-$version = Get-Content '.version'
+$version = Get-Content "$PSScriptRoot/.version"
 
 $msiUrl = "https://github.com/containers/podman/releases/download/v$version/podman-v$($version).msi"
 
-Invoke-WebRequest -Uri $msiUrl -Outfile install.msi
+Invoke-WebRequest -Uri $msiUrl -Outfile $PSScriptRoot/install.msi
 
-$checksum = Get-FileHash -Path install.msi
+$checksum = Get-FileHash -Path $PSScriptRoot/install.msi
 
 "version=$version" >> $env:GITHUB_ENV
 "checksum=$($checksum.Hash)" >> $env:GITHUB_ENV
